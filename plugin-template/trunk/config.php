a30e0fd90e0a58086d52637b155491ad236c262e
Daniel Sternlicht <gdblogst@gmail.com>
2022-06-10 10:43:18 +0300

    Adding optional parent config settings

--- plugin-template/trunk/config.php
+++ plugin-template/trunk/config.php
@@ -6,5 +6,10 @@ return array(
     // Plugin's name in Wordpress menu (required)
     'plugin_name' => 'My Plugin',
     // Plugin's icon url / base64 / relative path (optional)
-    'plugin_icon' => ''
+    'plugin_icon' => '',
+    // Parent menu settings (optional)
+    // 'parent_menu' => array(
+    //     'name' => 'Common Ninja',
+    //     'slug' => 'commonninja',
+    // ),
 );
